718d921ff7c670bc1ca8b8e3d412c10dc3501848
Devansh Patel <dp5401035@gmail.com>
2026-04-21 14:11:36 +0530

    Time: 5 ms (82.63%) | Memory: 46.6 MB (44.67%) - LeetSync

--- leetcode/32-longest-valid-parentheses/longest-valid-parentheses.java
+++ leetcode/32-longest-valid-parentheses/longest-valid-parentheses.java
@@ -4,15 +4,14 @@ class Solution {
         stack.push(-1);
         int maxLength = 0;
         for(int i=0 ; i<s.length(); i++){
-            if(s.charAt(i) =='('){
+            if(s.charAt(i) == '('){
                 stack.push(i);
-
             }else{
                 stack.pop();
                 if(stack.isEmpty()){
                     stack.push(i);
                 }else{
-                    maxLength = Math.max(maxLength , i - stack.peek());
+                    maxLength = Math.max(maxLength , i- stack.peek());
                 }
             }
         }
