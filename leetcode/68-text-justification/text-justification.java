0579d20f80610778431fb95871b6b0c166bc2abf
Devansh Patel <dp5401035@gmail.com>
2026-06-14 10:37:04 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (45.41%) - LeetSync

---
+++ leetcode/68-text-justification/text-justification.java
@@ -0,0 +1,64 @@
+class Solution {
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        List<String> result = new ArrayList<>();
+        int i = 0;
+
+        while (i < words.length) {
+            int lineLength = words[i].length();
+            int j = i + 1;
+
+            while (j < words.length &&
+                   lineLength + 1 + words[j].length() <= maxWidth) {
+                lineLength += 1 + words[j].length();
+                j++;
+            }
+
+            StringBuilder line = new StringBuilder();
+            int numWords = j - i;
+
+            if (j == words.length || numWords == 1) {
+                line.append(words[i]);
+
+                for (int k = i + 1; k < j; k++) {
+                    line.append(" ").append(words[k]);
+                }
+
+                while (line.length() < maxWidth) {
+                    line.append(" ");
+                }
+            } else {
+                int totalWordLength = 0;
+
+                for (int k = i; k < j; k++) {
+                    totalWordLength += words[k].length();
+                }
+
+                int totalSpaces = maxWidth - totalWordLength;
+                int gaps = numWords - 1;
+
+                int spacesPerGap = totalSpaces / gaps;
+                int extraSpaces = totalSpaces % gaps;
+
+                for (int k = i; k < j - 1; k++) {
+                    line.append(words[k]);
+
+                    for (int s = 0; s < spacesPerGap; s++) {
+                        line.append(" ");
+                    }
+
+                    if (extraSpaces > 0) {
+                        line.append(" ");
+                        extraSpaces--;
+                    }
+                }
+
+                line.append(words[j - 1]);
+            }
+
+            result.add(line.toString());
+            i = j;
+        }
+
+        return result;
+    }
+}
