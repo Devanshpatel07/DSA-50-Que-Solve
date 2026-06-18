e1f5e961db598b4027cfa98282f658bcf1aab3dc
Devansh Patel <dp5401035@gmail.com>
2026-06-18 14:42:42 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (41.38%) - LeetSync

--- leetcode/75-sort-colors/sort-colors.java
+++ leetcode/75-sort-colors/sort-colors.java
@@ -1,29 +1,22 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int low = 0;                  // Boundary pointer for 0s (Red)
-        int mid = 0;                  // Scanner pointer
-        int high = nums.length - 1;   // Boundary pointer for 2s (Blue)
-
-        while (mid <= high) {
-            if (nums[mid] == 0) {
-                // Found a 0: Swap it into the low boundary
-                swap(nums, low, mid);
-                low++;
-                mid++;
-            } else if (nums[mid] == 1) {
-                // Found a 1: It's already in the middle, just move past it
-                mid++;
-            } else if (nums[mid] == 2) {
-                // Found a 2: Swap it into the high boundary
-                swap(nums, mid, high);
-                high--;
-                // Note: Do NOT increment mid here! The element swapped from 
-                // 'high' is unexamined and needs to be checked in the next iteration.
-            }
+        int low =0;
+        int mid = 0;
+        int high = nums.length-1;
+        while(mid <= high){
+        if(nums[mid] == 0){
+            swap(nums , low , mid);
+            low++;
+            mid++;
+        }else if(nums[mid] == 1){
+            mid++;
+        }else if(nums[mid] == 2){
+            swap(nums , mid , high);
+            high--;
         }
     }
-
-    private void swap(int[] nums, int i, int j) {
+    }
+    private void swap(int nums[] , int i , int j){
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
