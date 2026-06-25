8491b89e926079f936e3060d27a3d60a7457ff46
Devansh Patel <dp5401035@gmail.com>
2026-06-25 10:29:22 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (49.68%) - LeetSync

--- leetcode/86-partition-list/partition-list.java
+++ leetcode/86-partition-list/partition-list.java
@@ -10,7 +10,7 @@
  */
 class Solution {
     public ListNode partition(ListNode head, int x) {
-        ListNode lessDummy =    new ListNode(0);
+        ListNode lessDummy = new ListNode(0);
         ListNode greaterDummy = new ListNode(0);
 
         ListNode less = lessDummy;
@@ -18,18 +18,18 @@ class Solution {
 
 
         while(head != null){
-            if(head.val < x){
+            if(head.val < x ){
                 less.next = head;
-                less = less.next;
+                less =  less.next; 
             }else{
                 greater.next = head;
-                greater = greater.next;
+                greater = greater.next; 
             }
             head = head.next;
         }
         greater.next = null;
         less.next = greaterDummy.next;
 
-        return lessDummy.next;
+            return lessDummy.next;
     }
 }
